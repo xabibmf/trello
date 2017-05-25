9bf0a7b123799417844810f9518bfe26b05f4ca1
xabibmf <xabibmf@gmail.com>
2017-05-25 13:46:06 +0900

    Add auto deploy setting

---
+++ config/deploy.rb
@@ -0,0 +1,21 @@
+lock "3.8.1"
+
+set :application, "trello"
+set :repo_url, "git@example.com:xabibmf/trello.git"
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vender/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.1'
+
+set :ssh_options, auth_methods: ['publickey'], keys: ['~/.ssh/key_pair.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
