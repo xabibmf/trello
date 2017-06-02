588b7d926adf3fb0e5af7276daa32770674a679a
xabibmf <xabibmf@gmail.com>
2017-06-02 16:35:42 +0900

    Add new board create view

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,7 +3,15 @@ $(document).on('turbolinks:load', function() {
     $('.pop-over').addClass("is-shown");
   });
 
-  $('.fa-times').on('click', function() {
+  $('.pop-over .fa-times').on('click', function() {
     $('.pop-over').removeClass("is-shown");
+  });
+
+  $('.new_board .board_link').on('click', function() {
+    $('.pop-over__board').addClass("is-shown");
+  });
+
+  $('.pop-over__board .fa-times').on('click', function() {
+    $('.pop-over__board').removeClass("is-shown");
   });
 });
