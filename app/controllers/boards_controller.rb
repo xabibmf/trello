2ed0dd7dbbef0d7a10a28bc2c671f2da0a8b3e5f
xabibmf <xabibmf@gmail.com>
2017-06-02 17:41:12 +0900

    Add board create function  to insert board table

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,14 @@
 class BoardsController < ApplicationController
   def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(name: params[:board][:name], manager: current_user.name)
+    if @board.save
+
+    else
+
+    end
   end
 end
