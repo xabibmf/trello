2aa44666daf7a183994ae95f4e12fc858c5b8147
xabibmf <xabibmf@gmail.com>
2017-06-02 17:52:20 +0900

    Remove column manager for board table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170602060029) do
+ActiveRecord::Schema.define(version: 20170602085000) do
 
   create_table "boards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "manager",                                    null: false
     t.string   "name",                                       null: false
     t.string   "contracting"
     t.string   "website"
