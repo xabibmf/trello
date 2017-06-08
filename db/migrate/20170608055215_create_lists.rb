d8f72a84974c0d361ffe1032a8332ab98985606d
xabibmf <xabibmf@gmail.com>
2017-06-08 15:03:46 +0900

    Add list table

--- db/migrate/20170608055215_create_lists.rb
+++ db/migrate/20170608055215_create_lists.rb
@@ -1,7 +1,8 @@
 class CreateLists < ActiveRecord::Migration[5.0]
   def change
     create_table :lists do |t|
-
+      t.string :name, null: false
+      t.references :board, null: false, foreign_key: true
       t.timestamps
     end
   end
