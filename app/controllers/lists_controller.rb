c9b0b334e0098d0991f213171e0f694d01cb6a13
xabibmf <xabibmf@gmail.com>
2017-06-09 12:44:29 +0900

    Add list create view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,27 @@
 class ListsController < ApplicationController
+  before_action :set_board, only: [:new, :create]
+
   def new
+    @list = List.new
+  end
+
+  def create
+    binding.pry
+    @list = List.new(list_params)
+    if @list.save
+
+    else
+
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name).merge(board: @board)
+  end
+
+  def set_board
     @board = Board.find(params[:board_id])
   end
 end
