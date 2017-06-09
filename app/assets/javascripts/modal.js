08387450a14dc2beb35f7f91b6ae6e277f80c26f
xabibmf <xabibmf@gmail.com>
2017-06-09 19:54:22 +0900

    Add modal close action

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,7 +1,25 @@
 $(document).on('turbolinks:load', function() {
+  var mask = $('.mask');
+  var modal = $('.modal');
+  var times = $('.modal-close');
+
+  function maskClose() {
+    mask.addClass('hidden');
+    modal.addClass('hidden');
+  }
+
   $('.lists__item__content__card').each(function() {
     $(this).on('click', function() {
-      $('.mask').removeClass('hidden');
+      mask.removeClass('hidden');
+      modal.removeClass('hidden');
     });
+  });
+
+  times.on('click', function() {
+    maskClose();
+  });
+
+  mask.on('click', function() {
+    maskClose();
   });
 });
