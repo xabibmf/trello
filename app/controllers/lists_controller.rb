afba954854d1a44fafa399c8c7da9edad0829318
xabibmf <xabibmf@gmail.com>
2017-06-09 14:49:37 +0900

    Add show all list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,12 +1,12 @@
 class ListsController < ApplicationController
   before_action :set_board, only: [:new, :create]
+  before_action :set_lists, only: :new
 
   def new
     @list = List.new
   end
 
   def create
-    binding.pry
     @list = List.new(list_params)
     if @list.save
 
@@ -23,5 +23,9 @@ class ListsController < ApplicationController
 
   def set_board
     @board = Board.find(params[:board_id])
+  end
+
+  def set_lists
+    @lists = @board.lists
   end
 end
