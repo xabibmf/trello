2a21a6e5561a0f57e3e657cb8c41f10e032fe9fc
xabibmf <xabibmf@gmail.com>
2017-06-09 14:39:57 +0900

    Modify association and function after list create

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -1,9 +1,14 @@
 $(document).on('turbolinks:load', function() {
+  function clearPopoverList() {
+    $('#list_name').val('');
+    $('.pop-over__list').removeClass('is-shown');
+  }
+
   $('.lists__item__newlist__link').on('click', function() {
     $('.pop-over__list').addClass('is-shown');
   });
 
   $('.popover-list-close').on('click', function() {
-    $('.pop-over__list').removeClass('is-shown');
+    clearPopoverList();
   });
 });
