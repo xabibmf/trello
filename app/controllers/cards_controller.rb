90d13c7708cde2243c0d33eaaa65579c2dfd64e1
xabibmf <xabibmf@gmail.com>
2017-06-09 17:13:01 +0900

    Add insert into card table function

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,14 @@
 class CardsController < ApplicationController
+  def create
+    @list = Card.new(card_params)
+    if @list.save
+    else
+    end
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:name).merge(params.permit(:list_id))
+  end
 end
