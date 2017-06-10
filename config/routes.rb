0679c1d0e2d2ae613b896a496a7394cab24244c6
xabibmf <xabibmf@gmail.com>
2017-06-10 16:38:51 +0900

    Add edit list name form view and style

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'boards#new'
   resources :boards, only: :create do
-    resources :lists, only: [:new, :create] do
+    resources :lists, only: [:new, :create, :edit] do
       resources :cards, only: :create do
         resources :activities, only: :create
       end
