592f92f1ce0e90d21fc1d467d25d504eba8ffb76
xabibmf <xabibmf@gmail.com>
2017-06-10 12:42:16 +0900

    Add activity memo form view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root 'boards#new'
   resources :boards, only: :create do
     resources :lists, only: [:new, :create] do
-      resources :cards, only: :create
+      resources :cards, only: :create do
+        resources :activities, only: :create
+      end
     end
   end
 end
