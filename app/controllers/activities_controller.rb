a18e8bd4705db0ec9f217a51369ecda984ad617d
xabibmf <xabibmf@gmail.com>
2017-06-10 14:13:38 +0900

    Add activity log insert table function

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,14 @@
 class ActivitiesController < ApplicationController
   def create
+    @activity = Activity.new(activity_params)
+    if @activity.save
+    else
+    end
+  end
 
+  private
+
+  def activity_params
+    params.require(:activity).permit(:memo).merge(params.permit(:card_id))
   end
 end
