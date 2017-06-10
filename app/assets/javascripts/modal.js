ef222c6853f32d93795f234ae3456bf8900c4088
xabibmf <xabibmf@gmail.com>
2017-06-10 15:02:41 +0900

    Add show activity log

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,8 +1,6 @@
 $(document).on('turbolinks:load', function() {
   var mask = $('.mask');
   var times = $('.modal-close');
-  var activityForm = $('#activity_memo');
-  var submit = $('.activity-submit-button');
 
   function maskClose() {
     mask.addClass('hidden');
@@ -10,9 +8,24 @@ $(document).on('turbolinks:load', function() {
   }
 
   $('.lists__item__content__card').each(function() {
+    var activityForm = $(this).next().find('#activity_memo');
+    var submit = $(this).next().find('.activity-submit-button');
+    var form = $(this).next().find('#new_activity');
     $(this).on('click', function() {
       mask.removeClass('hidden');
       $(this).next().removeClass('hidden');
+    });
+    activityForm.on('keyup', function() {
+      if ($(this).val().length != 0) {
+        submit.addClass('active');
+      } else {
+        submit.removeClass('active');
+      }
+    });
+    form.on('submit', function() {
+      if (!submit.hasClass('active')) {
+        return false;
+      }
     });
   });
 
@@ -22,19 +35,5 @@ $(document).on('turbolinks:load', function() {
 
   mask.on('click', function() {
     maskClose();
-  });
-
-  activityForm.on('keyup', function() {
-    if ($(this).val().length != 0) {
-      submit.addClass('active');
-    } else {
-      submit.removeClass('active');
-    }
-  });
-
-  submit.on('submit', function() {
-    if (!$(this).hasClass('active')) {
-      return false;
-    }
   });
 });
