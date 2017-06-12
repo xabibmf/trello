ae6c8eb25f78c0fadee582219dbed256c880c417
xabibmf <xabibmf@gmail.com>
2017-06-12 20:41:33 +0900

    Modify create board ajax

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -5,13 +5,12 @@ $(document).on('turbolinks:load', function() {
     return html;
   }
 
-  $('.new_board').on('submit', function(e) {
+  $('#new_board').on('submit', function(e) {
     var nameField = $('#board_name');
     if(!nameField.val()) {
       return;
     }
-    var test = '#board_name';
-    var formData = new FormData($(this).get(0));
+    var formData = new FormData($('#new_board').get(0));
     e.preventDefault();
     $.ajax({
       type: 'POST',
@@ -24,13 +23,11 @@ $(document).on('turbolinks:load', function() {
     .done(function(data) {
       var html = buildBoardHTML(data);
       nameField.val('');
-      var boardColor = '.color-selector_item.' + data.color + '-content';
-      console.log($(boardColor));
-      $(boardColor).removeClass(boardColor);
+      $('.color-checked').removeClass('color-checked');
       $('.board-list').children('.new_board').before(html);
     })
     .fail(function() {
-
+      alert("fail to create board");
     })
     .always(function() {
       $(this).attr('disabled', false);
