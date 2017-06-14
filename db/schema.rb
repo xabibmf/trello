eb17d4fea9fb011682df537463a7e956b903eb2a
xabibmf <xabibmf@gmail.com>
2017-06-14 20:07:32 +0900

    Add position column to card table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614053417) do
+ActiveRecord::Schema.define(version: 20170614105822) do
 
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "memo",       limit: 65535, null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20170614053417) do
     t.integer  "list_id",                  null: false
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "position"
     t.index ["list_id"], name: "index_cards_on_list_id", using: :btree
   end
 
