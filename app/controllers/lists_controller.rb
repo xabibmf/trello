9735b8b6f2b2c106abc70542e3772ff9cdbbd708
xabibmf <xabibmf@gmail.com>
2017-06-15 14:21:55 +0900

    Add list delete function

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -25,6 +25,19 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    list = List.find(delete_params[:id])
+    list.cards.each do |card|
+      card.activities.destroy_all
+    end
+    list.cards.destroy_all
+    list.move_to_bottom
+    list.delete
+    @list = List.new
+    @card = Card.new
+    @activity = Activity.new
+  end
+
   def sort
     target_list = List.find(sort_params[:list_id])
     target_list.insert_at(sort_params[:position].to_i + 1)
@@ -39,6 +52,10 @@ class ListsController < ApplicationController
 
   def sort_params
     params.permit(:board_id, :list_id, :position)
+  end
+
+  def delete_params
+    params.permit(:id)
   end
 
   def set_board
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'boards#new'
   resources :boards, only: [:create, :update, :destroy] do
-    resources :lists, only: [:new, :create, :update] do
+    resources :lists, only: [:new, :create, :update, :destroy] do
       put :sort
       resources :cards, only: [:create, :update] do
         put :sort
