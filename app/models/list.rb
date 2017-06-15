49939c404dd691e04c41ac9909c39126cc204a4b
xabibmf <xabibmf@gmail.com>
2017-06-15 13:22:16 +0900

    Add association for destroy model

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,5 @@
 class List < ApplicationRecord
   belongs_to :board
   acts_as_list scope: :board
-  has_many :cards, -> { order(position: :asc) }
+  has_many :cards, -> { order(position: :asc) }, dependent: :destroy
 end
