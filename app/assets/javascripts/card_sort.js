ae2c0f851a7fbb275340a12f1a01599c0ae27d2b
xabibmf <xabibmf@gmail.com>
2017-06-15 20:39:37 +0900

    Change drag and drop handle

--- app/assets/javascripts/card_sort.js
+++ app/assets/javascripts/card_sort.js
@@ -3,14 +3,14 @@ $(document).on('turbolinks:load', function() {
     items: $('.card'),
     tolerance: "pointer",
     placeholder: "emptycard",
-    connectWith: $('.lists__item__content'),
+    // connectWith: $('.lists__item__content'),
+    handle: $('.lists__item__content__card'),
     update: function(e, ui) {
       item = ui.item;
       item_data = item.data();
       $.ajax({
         type: 'PUT',
         url: item_data.updateUrl,
-        dataType: 'json',
         data: { position: item.index(), new_list_id: item.parent().parent().attr('id') }
       });
     }
--- app/assets/javascripts/list_sort.js
+++ app/assets/javascripts/list_sort.js
@@ -2,6 +2,7 @@ $(document).on('turbolinks:load', function() {
   $('.lists').sortable({
     items: $('.lists__item:not(:last-child)'),
     tolerance: "pointer",
+    handle: $('.lists__item__header'),
     update: function(e, ui) {
       item = ui.item;
       item_data = item.data();
