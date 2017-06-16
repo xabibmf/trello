5b994021d4a07edf0f385b0772d21764d90c851e
xabibmf <xabibmf@gmail.com>
2017-06-16 13:01:31 +0900

    Fix moving card to list with no card

--- app/assets/javascripts/card_sort.js
+++ app/assets/javascripts/card_sort.js
@@ -4,6 +4,22 @@ $(document).on('turbolinks:load', function() {
     tolerance: "pointer",
     placeholder: "emptycard",
     handle: $('.lists__item__content__card'),
+    start: function(e, ui) {
+      $('.lists__item').each(function() {
+        if ($(this).find('.card').length == 1 && $(this).find('.hiddena').length == 0) {
+          $(this).find('.lists__item__content').prepend('<div class="card hiddena"><div class="lists__item__content__card"></div></div>');
+        }
+      });
+    },
+    beforeStop: function(e, ui) {
+      $('.lists__item').each(function() {
+        if ($(this).find('.card').length > 2) {
+          $(this).find('.hiddena').remove();
+        } else if ($(this).find('.card').length == 1　&& $(this).find('.hiddena').length == 0) {
+          $(this).find('.lists__item__content').prepend('<div class="card hiddena"><div class="lists__item__content__card"></div></div>');
+        }
+      });
+    },
     update: function(e, ui) {
       item = ui.item;
       item_data = item.data();
